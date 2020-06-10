1add1cc4b36ec427b16de048add049145a2528cb
Denis Ilchishin <ilchishin.denis@gmail.com>
2020-06-10 10:20:54 +0800

    Added BlogPost model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -16,6 +16,16 @@ class Blog(models.Model):
         verbose_name_plural = _('blogs')
 
 
+class BlogPost(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, verbose_name=_('blog'))
+    title = models.CharField(_('post title'), max_length=255)
+    text = models.TextField(_('post text'))
+
+    class Meta:
+        verbose_name = _('post')
+        verbose_name_plural = _('posts')
+
+
 @receiver(post_save, sender=get_user_model())
 def user_post_create(instance, created, **kwargs):
     if created:
