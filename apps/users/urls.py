6fd14384039e21b818d82f95937162197686d07f
Denis Ilchishin <ilchishin.denis@gmail.com>
2020-06-11 11:05:58 +0800

    Added subscribe/unsubscribe to blog feature

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+from django.urls import include, path
 
-from .views import AccountView
+from .views import AccountView, SubscribeView, UnsubscribeView
 
 urlpatterns = [
     path('', AccountView.as_view(), name='account'),
+    path(
+        'api/',
+        include(
+            [
+                path('subscribe/', SubscribeView.as_view(), name='api-subscribe'),
+                path('unsubscribe/', UnsubscribeView.as_view(), name='api-unsubscribe'),
+            ]
+        ),
+    ),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,32 @@
 from django.contrib.auth.decorators import login_required
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
 from django.utils.decorators import method_decorator
+from django.views import View
 from django.views.generic import TemplateView
+
+from apps.blog.models import Blog
 
 
 @method_decorator(login_required, name='get')
 class AccountView(TemplateView):
     template_name = 'users/account.html'
+
+
+@method_decorator(login_required, name='post')
+class SubscribeView(View):
+    def post(self, request, *args, **kwargs):
+        pk = request.POST.get('pk')
+        blog = get_object_or_404(Blog, pk=pk)
+        request.user.subscriptions.add(blog)
+        return HttpResponseRedirect(reverse('blog-list'))
+
+
+@method_decorator(login_required, name='post')
+class UnsubscribeView(View):
+    def post(self, request, *args, **kwargs):
+        pk = request.POST.get('pk')
+        blog = get_object_or_404(Blog, pk=pk)
+        request.user.subscriptions.remove(blog)
+        return HttpResponseRedirect(reverse('blog-list'))
