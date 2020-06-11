e648d01dba9fa3dbe2891a3a840258bd051d2f43
Denis Ilchishin <ilchishin.denis@gmail.com>
2020-06-11 09:22:52 +0800

    Added login and logout views

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,19 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView as DefaultLoginView
+from django.contrib.auth.views import LogoutView as DefaultLogoutView
+from django.utils.decorators import method_decorator
 from django.views.generic import RedirectView
 
 
 class HomeView(RedirectView):
     pattern_name = 'blog-list'
+
+
+class LoginView(DefaultLoginView):
+    template_name = 'login.html'
+
+
+@method_decorator(login_required, name='get')
+@method_decorator(login_required, name='post')
+class LogoutView(DefaultLogoutView):
+    pass
--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogs/', include('apps.blog.urls')),
+    path('account/', include('apps.users.urls')),
     path('', include('apps.core.urls')),
 ]
